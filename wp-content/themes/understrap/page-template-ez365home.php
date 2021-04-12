9dcf751d0e5c92a1bf78433248ebee8d3afa7c81
Daniel <daniel.n.labonte@gmail.com>
2021-04-11 22:59:18 -0400

    added home page and all assets

---
+++ wp-content/themes/understrap/page-template-ez365home.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * Template Name: EZ365 Home
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+<!-- Cover -->
+  <div class="py-5 text-center section-parallax section-dark" style="background-image: url(/wp-content/uploads/assets/Header-full.jpg);">
+    <!-- <div class="pt-5 bg-primary section-parallax" style="background: url(/wp-content/uploads/assets/Header-full.jpg) no-repeat center center;"> -->
+    <div class="container mv-5 p-5">
+      <div class="row text-white">
+        <div class="col-md-12 my-5 text-lg-left text-center align-self-center d-flex flex-column justify-content-center align-items-center">
+          <span class="lead text-uppercase" style="">Revolutionary Digital Asset Ecosystem</span>
+          <h1 class="display-2" style="font-weight: 400">Play. Trade. Learn.</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Article style section -->
+  <div class="mt-5">
+    <div class="container d-flex flex-column align-items-center">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="mb-3"><b>Learn how EZ365 is changing the digital asset landscape</b></h3>
+          <div class="embed-responsive embed-responsive-16by9 px-5">
+            <iframe src="https://www.youtube.com/embed/bStX0I5PWmQ?controls=0" allowfullscreen="" class="embed-responsive-item"></iframe>
+          </div>
+        </div>
+      </div>
+      <hr class="">
+      <hr class="mt-0" style="background-color: red; height: 10px; color: blue;">
+    </div>
+  </div>
+  <div>
+    <div class="container">
+      <hr>
+    </div>
+  </div>
+  <div class="my-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 d-flex flex-column align-items-center">
+          <h3 class=""><b>Be in the know.</b></h3>
+          <form class="d-flex flex-column align-items-center">
+            <div class="form-group">
+              <label class="mb-0">Email *</label>
+              <input type="email" class="form-control rounded-0" placeholder="Email Address">
+              <small class="form-text text-muted">*By signing up, you agree to receive updates and marketing emails from EZ365.</small>
+            </div>
+            <button type="submit" class="btn text-uppercase btn-primary text-light rounded-0 py-2 px-3">Sign Up Now</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Secion 1 -->
+  <div class="py-5 text-center section-dark" style="background-image: url(/wp-content/uploads/assets/pattern-3232784_1920b.jpg);">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="mb-0 text-white stretch text-uppercase">Facilitating the Mass Adoption of Cryptocurrency.</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="my-5" id="features">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p class="pb-4 mx-5 px-5" contenteditable="true">EZ365 is a digital ecosystem that combines the best aspects of NFTs, online gaming, digital asset trading and blockchain education. The EZ365 experience is simple, user friendly and fun, so you can easily play, trade and learn with digital assets. </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="align-self-center text-center col-lg-4 col-md-4 d-flex flex-column align-items-center px-3 my-4">
+          <h4 class="mb-3 text-dark"><b>Play.</b></h4>
+          <p class="mb-3 text-uppercase"><b>A provably fair, effortless way to play and bet with cryptocurrency.</b></p>
+          <p class="mb-3 text-md-left" style="">A casino you can trust because you can see the outcome of every play. EZ Win attracts a community with top casino games, plus betting for sports and e-sports. Customers can play a stable coin pegged to the US dollar.</p>
+          <a class="btn btn-outline-dark rounded-0 text-uppercase py-2 px-3" href="#">Learn More</a>
+        </div>
+        <div class="align-self-center text-center col-lg-4 col-md-4 d-flex flex-column align-items-center px-3 my-4">
+          <h4 class="mb-3 text-dark"><b>Trade.</b></h4>
+          <p class="mb-3 text-uppercase"><b>Enabling new and expert users to invest in NFTs and cryptocurrency.</b></p>
+          <p class="mb-3 text-md-left" style="">An exchange platform dedicated to new and expert traders. EZ Exchange is intuitive, fully supported and fiat-enabled. Trade digital assets such as non-fungible tokens (NFTs) and cryptocurrency with the click of a button.</p>
+          <a class="btn btn-outline-dark rounded-0 text-uppercase py-2 px-3" href="#">Learn More</a>
+        </div>
+        <div class="align-self-center text-center col-lg-4 col-md-4 d-flex flex-column align-items-center px-3 my-4">
+          <h4 class="mb-3 text-dark"><b>Learn.</b></h4>
+          <p class="mb-3 text-uppercase"><b>helping the world embrace blockchain.</b></p>
+          <p class="mb-3 text-md-left" style="">An all-in-one educational portal that helps users take their blockchain knowledge to the next level. EZ Academy offers instruction and insight on a wide range of topics throughout the blockchain and digital asset universe.</p>
+          <a class="btn btn-outline-dark rounded-0 text-uppercase py-2 px-3" href="#">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="my-5" id="features">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h3 class="">The EZ365 Platform</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12"><img class="img-fluid d-block" src="/wp-content/uploads/assets/the_ez365_platform.png"></div>
+      </div>
+    </div>
+  </div>
+  <!-- Section 2 -->
+  <div class="py-5 text-center section-dark" style="background-image: url(/wp-content/uploads/assets/stock-graph.jpg);">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="display-5 mb-0 text-white stretch text-uppercase">Built by Professionals Who Protect the World’s Stock Markets.</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="my-5" id="features">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p class="p-0 mx-5">EZ365 brings the rigor of established technology and financial market security to our digital asset platforms. From server redundancy Blockchain optimization, fault tolerance and high availability systems, to Know Your Customer (KYC)/ Anti-Money Laundering (AML) compliance, EZ365 has the expertise and experience to enable users to play, trade and learn safely and securely.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Section 3 -->
+  <div class="py-5 text-center section-dark" style="background-image: url(/wp-content/uploads/assets/markus-spiske-unsplash.jpg);">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="display-5 mb-0 text-white stretch text-uppercase">Stay Up To Date with the Latest from EZ365.</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="my-5" id="features">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <a href="#" style="text-decoration: none;" class="text-dark">
+          </a>
+          <div class="card">
+            <a href="#" style="text-decoration: none;" class="text-dark">
+              <img class="card-img-top" src="/wp-content/uploads/assets/Logo.png" alt="Card image cap">
+            </a>
+            <div class="card-body bg-light d-flex flex-column align-items-center">
+              <a href="#" style="text-decoration: none;" class="text-dark">
+                <h4 class="card-title text-center">EZ365 Update: March 2020</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </a><a class="btn btn-link" href="#">Read more -&gt;</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="/wp-content/uploads/assets/Logo.png" alt="Card image cap">
+            <div class="card-body bg-light d-flex flex-column align-items-center">
+              <h4 class="card-title text-center">EZ365 Update: March 2020</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a class="btn btn-link" href="#">Read more -&gt;</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="/wp-content/uploads/assets/Logo.png" alt="Card image cap">
+            <div class="card-body bg-light d-flex flex-column align-items-center">
+              <h4 class="card-title text-center">EZ365 Update: March 2020</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a class="btn btn-link" href="#">Read more -&gt;</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="my-3 col-md-12 d-flex flex-column align-items-center">
+          <a class="btn btn-outline-dark rounded-0 text-uppercase py-2 px-3" href="#">More EZ365 News</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="my-5">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="d-flex align-items-center flex-column"><b>As Seen In</b></h3>
+        </div>
+      </div>
+      <div class="row d-flex justify-content-center px-4">
+        <div class="col-6 col-lg-3 col-xl-2"><img class="img-fluid d-block" src="/wp-content/uploads/assets/Venture_Beat.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/BlockTribune.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/Bitport.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/DMW.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/Financial_IT.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/Payment_Expert.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/PCNewsBuzz.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/Pymnts.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/Securities.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/Toolbox_Tech.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" src="/wp-content/uploads/assets/Wired_Focus.png"></div>
+      </div>
+    </div>
+  </div>
+  <!-- Section 4 -->
+  <div class="py-5 text-center section-dark" style="background-image: url(/wp-content/uploads/assets/hector-j-rivas-unsplashb.jpg);">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="display-5 mb-0 text-white stretch text-uppercase">Partnering with Leading Companies in the Blockchain Industry.</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="my-5">
+    <div class="container-fluid">
+      <div class="row d-flex justify-content-center px-4">
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" style="object-fit: contain;" src="/wp-content/uploads/assets/CertiK.png"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" style="object-fit: contain;" src="/wp-content/uploads/assets/KABN_Logo.jpeg"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" style="object-fit: contain;" src="/wp-content/uploads/assets/Shyft.jpg"></div>
+        <div class="col-6 col-lg-3 col-xl-2 d-flex justify-content-center"><img class="img-fluid d-block" style="object-fit: contain;" src="/wp-content/uploads/assets/Digimax.jpg"></div>
+      </div>
+    </div>
+  </div>
+  <div class="mt-5 bg-info">
+    <div class="container p-5">
+      <div class="row">
+        <div class="col-md-12 text-lg-left text-center align-self-center d-flex flex-column justify-content-center align-items-center my-0">
+          <p class="text-center" style="">Don’t be left in the dark. Sign up for our newsletter today!</p>
+          <form class="form-inline w-100" style="max-width: 450px">
+            <div class="input-group w-100">
+              <input type="text" class="form-control py-4 rounded-0" placeholder="Email Address">
+              <div class="input-group-append">
+                <button class="btn btn-dark px-3 text-uppercase rounded-0" type="button">Subscribe Now</button>
+              </div>
+            </div>
+            <small class="form-text text-muted mt-3 text-center" style="font-size: 12;"> By subscribing to our newsletter, you agree to receive marketing emails from EZ365. </small>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php
+get_footer();
